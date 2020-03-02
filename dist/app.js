5afa0daf367b76034e18978bb4628dcaa0d32154
Christian Felipe <bupeeee@gmail.com>
2020-03-03 03:57:40 +0800

    update, add dropdown menu (not finished)

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,15 @@
 const navToggle = document.getElementById('nav_toggle_button');
 const navDesktop = document.getElementById('nav_desktop');
 
+const navLink = document.getElementsByClassName('nav_link');
+
 navToggle.addEventListener('click', () => {
     navDesktop.classList.toggle('active')
+})
+
+Array.from(navLink).forEach(function(navlink){
+    navlink.addEventListener('click', (e) => {
+        e.preventDefault();
+
+    })
 })
